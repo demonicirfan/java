dcbd7011fa928d0b4bbe4cc585e1c69c69e0ac2d
demonicirfan <hashtag.irfan@gmail.com>
2021-08-06 16:13:53 +0530

    first program

---
+++ arithematic/Arithematic.java
@@ -0,0 +1,13 @@
+package arithematic;
+
+public class Arithematic{
+    public static void main(String[] args) {
+        int a = 14, b = 15;
+        int c = a / b;
+        int r = a % b;
+
+        System.out.println(c);
+        System.out.println(r);
+
+    }
+}

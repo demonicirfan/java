e66bf6721e383e2b20e402cc2f9899b4fc833f69
demonicirfan <hashtag.irfan@gmail.com>
2021-08-07 11:59:14 +0530

    arithematic

--- arithematic/Arithematic.java
+++ arithematic/Arithematic.java
@@ -1,12 +1,18 @@
 package arithematic;
 
-public class Arithematic{
+public class Arithematic {
     public static void main(String[] args) {
-        int a = 14, b = 15;
-        int c = a / b;
-        int r = a % b;
+        // int a = 14, b = 15;
+        // int c = a / b;
+        // int r = a % b;
 
-        System.out.println(c);
+        // System.out.println(c);
+        // System.out.println(r);
+
+        float a = 20.3f, b = 3.2f;
+
+        float r = a % b;
+
         System.out.println(r);
 
     }

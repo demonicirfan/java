313473ecde25f62b80ed590210df913ce4dca8b2
demonicirfan <hashtag.irfan@gmail.com>
2021-08-08 09:48:22 +0530

    area of a triangle

---
+++ area/Area.java
@@ -0,0 +1,18 @@
+package area;
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        float base, height, area;
+        System.out.println("Enter Base and Height ");
+        Scanner sc = new Scanner(System.in);
+        base = sc.nextFloat();
+        height = sc.nextFloat();
+
+        area = base * height / 2;
+
+        System.out.println("Area of the tringale is " + area);
+    }
+}

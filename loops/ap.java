95527cbde28732ab058c3cd4d3587eae913fcf0c
demonicirfan <hashtag.irfan@gmail.com>
2021-08-10 15:26:21 +0530

    ap

---
+++ loops/ap.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class ap {
+    public static void main(String[] args) {
+        int a=1, d=5;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a + i*d);
+        }
+    }
+}

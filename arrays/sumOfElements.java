b7664515085701a92c12832be20beb3127f724fc
demonicirfan <hashtag.irfan@gmail.com>
2021-08-10 21:06:59 +0530

    arrays 2

--- arrays/sumOfElements.java
+++ arrays/sumOfElements.java
@@ -1,11 +1,9 @@
 package arrays;
 
-import java.time.temporal.IsoFields;
-
 public class sumOfElements {
     public static void main(String[] args) {
         int A[] = { 3, 9, 7, 8, 12, 6, 15, 5, 4, 10 };
-        int sum = 0;
+       /* int sum = 0;
         for (int i = 0; i < A.length; i++) {
             sum = sum + A[i];
         }
@@ -15,26 +13,27 @@ public class sumOfElements {
         int k = 8;
         for (int i = 0; i < A.length; i++) {
             if (A[i] == k) {
-                System.out.println("element found at " + i);
+                // System.out.println("element found at " + i);
                 System.exit(0);
             }
         }
         System.out.println("Not found");
-
-        int A[] = { 3, 9, 7, 8, 12, 6, 15, 5, 4, 10 };
-
+       */
         // finding maximum element
         int maxNum1 = 0;
         int maxNum2 = 0;
 
         for (int i = 0; i < A.length; i++) {
             if (maxNum1 < A[i]) {
+                maxNum2 = maxNum1;
                 maxNum1 = A[i];
-                if (maxNum2 < maxNum1) {
-                    maxNum2 = A[i];
-                }
+
+            } else if (maxNum2 < A[i]) {
+                maxNum2 = A[i];
             }
+
         }
-        System.out.println(maxNum1);
+        System.out.println("Largest is " + maxNum1);
+        System.out.println("second Largest is " + maxNum2);
     }
 }

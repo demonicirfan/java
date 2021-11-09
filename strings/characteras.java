ab037262230eda53bf0a36a49065f64261213937
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-10 01:05:01 +0530

    ✌

---
+++ strings/characteras.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class characteras {
+    public static void main(String[] args) {
+        System.out.println("Kunal " + new ArrayList<>());
+      //  System.out.println("kunal " + new Integer(56));
+    }
+}

0a713f2e1e897a3a5f77fe66d4cc8a2257c414b1
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-10 01:02:01 +0530

    ✌

--- arraylist/first.java
+++ arraylist/first.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class first {
     public static void main(String[] args) {
-        // syntax
+        Scanner scn = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>();
 
-        list.add(123);
-        list.add(153);
-        list.add(15);
-        list.add(6);
-        list.add(7);
+        // list.add(123);
+        // list.add(153);
+        // list.add(15);
+        // list.add(6);
+        // list.add(7);
+        // list.set(2, 555);
+        // list.remove(15);
+        
+        for (int i = 0; i < 10; i++) {
+            list.add(scn.nextInt());
+        }
 
         System.out.println(list);
 

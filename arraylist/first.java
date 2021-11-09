2f9a382569bb99c2a85cdd833801ae03433341bd
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-10 01:01:01 +0530

    ✌

---
+++ arraylist/first.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class first {
+    public static void main(String[] args) {
+        // syntax
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(123);
+        list.add(153);
+        list.add(15);
+        list.add(6);
+        list.add(7);
+
+        System.out.println(list);
+
+    }
+}

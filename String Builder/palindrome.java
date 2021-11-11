0c515fc134090650d05f2e8106f18f644c76184b
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-12 01:07:01 +0530

    ✌

--- String Builder/palindrome.java
+++ String Builder/palindrome.java
@@ -1,3 +1,18 @@
 public class palindrome {
-    
+    public static void main(String[] args) {
+
+    }
+
+    static boolean isPalendrome(String str) {
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length() / 2; i++) {
+            char head = str.charAt(i);
+            char tail = str.charAt(str.length() - 1 - i);
+            if (head != tail) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 }

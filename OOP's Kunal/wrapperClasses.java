fdef9924400ea996ffd26a0232fa9f78921e3753
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-19 01:07:01 +0530

    updated

--- OOP's Kunal/wrapperClasses.java
+++ OOP's Kunal/wrapperClasses.java
@@ -1,6 +1,9 @@
 public class wrapperClasses {
     public static void main(String[] args) {
         final StudentClass Irfan = new StudentClass();
+        Irfan.rollNumber = 33;
+        Irfan.name = "Irfan";
+        Irfan.numOfSubjects = 6;
     }
 }
 

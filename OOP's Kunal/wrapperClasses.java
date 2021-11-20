d024820a53e03cf810df9b13ee39434b50db2c92
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-21 01:07:01 +0530

    update

---
+++ OOP's Kunal/wrapperClasses.java
@@ -0,0 +1,11 @@
+public class wrapperClasses {
+    public static void main(String[] args) {
+        final int INCREASE = 10;
+        final Student Irfan = new Student();
+        Irfan.name = "Irfan";
+    }
+}
+
+class student {
+    String name;
+}

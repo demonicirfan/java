6ce522cdfb4383d2648f99c9e7adc667af8e2162
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-24 22:58:37 +0530

    multilevel inhertience

--- OOps/Inheritance/BoxWeight.java
+++ OOps/Inheritance/BoxWeight.java
@@ -7,6 +7,17 @@ public class BoxWeight extends Box {
         this.weight = -2;
     }
 
+    public BoxWeight(BoxWeight other) {
+        super(other);
+        weight = other.weight;
+    }
+
+    public BoxWeight(double side, double weight) {
+        super(side);
+        this.weight = weight;
+
+    }
+
     public BoxWeight(double l, double h, double w, double weight) {
         super(l, h, w); // call the parent class constructor
         this.weight = weight;
--- OOps/Inheritance/Main.java
+++ OOps/Inheritance/Main.java
@@ -2,12 +2,15 @@ package Inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box(4, 6, 9);
-        Box box2 = new Box(box);
-        System.out.println(box2.l + " " + box2.w + " " + box2.h);
+        // Box box = new Box(4, 6, 9);
+        // Box box2 = new Box(box);
+        // System.out.println(box2.l + " " + box2.w + " " + box2.h);
 
-        BoxWeight box3 = new BoxWeight();
-        System.out.println(box3.h + " " + box3.weight);
+        // BoxWeight box3 = new BoxWeight();
+        // System.out.println(box3.h + " " + box3.weight);
+
+        BoxPrice box = new BoxPrice(5, 5, 200);
+
     }
 
 }

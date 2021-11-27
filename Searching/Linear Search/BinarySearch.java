83617e9ee445c913919998e201df875d72268e1b
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-27 10:53:19 +0530

    order ognostic binary search

--- Searching/Linear Search/BinarySearch.java
+++ Searching/Linear Search/BinarySearch.java
@@ -1,7 +1,9 @@
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = { -18, -14, -5, 0, 9, 12, 66, 123, 453, 500 };
-        System.out.println(binarySearch(arr, 9));
+        System.out.println(binarySearch(arr, 453));
+        int[] arr2 = { 45, 23, 22, 9, 7, 6, 1, 0, -1, -5, -88 };
+        System.out.println(orderAgnosticBS(arr2, 0));
     }
 
     // retiurn the index
@@ -11,14 +13,48 @@ public class BinarySearch {
         while (start <= end) {
             // find the middle element
             int mid = start + (end - start) / 2;
+            // if target is less than middle element then the end will be mid -1
             if (target < arr[mid]) {
                 end = mid - 1;
             } else if (target > arr[mid]) {
                 start = mid + 1;
             } else {
-                return mid;
+                return mid;// if the mid element is the target then return mid
             }
         }
+        return -1;
+    }
+
+    static int orderAgnosticBS(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        // find whether the array is sorted in ascending or decending
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
         return -1;
     }
 }
